80e4a721fd2e85b6f85334e086d8719f392678ad
KD KOUSHIK <87390853+kdkoushik@users.noreply.github.com>
2024-04-17 19:40:22 +0600

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,24 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = '21352815'
+API_HASH = '7a0789ace80a08cdeaf1c496b4f884d9'
+BOT_TOKEN = '6884391979:AAFmYaafWAgn5XsFfGB3Hh98s7pB89xPpa8'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [6927862127, '6927862127', 6927862127]
+CHANNELS = [-1002045684382, -1002045684382, 'channelusername']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://koushik:iam@koushik@cluster0.ermeveq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = 'koushik'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+
